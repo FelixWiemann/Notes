d7cfab70af5c535e558fc10af6393338519d7b65
Felix Wiemann <felix.wiemann@de.abb.com>
2017-09-23 14:04:23 +0200

    mainactivity: handle activity lifecycle and storing/reading notes, changed main + main activity to comply to changed methodsignatures

--- app/src/main/java/com/example/felix/notizen/MainActivity.java
+++ app/src/main/java/com/example/felix/notizen/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.felix.notizen;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.felix.notizen.BackEnd.JsonManager.cJsonManager;
+import com.example.felix.notizen.BackEnd.JsonManager.cJsonManagerException;
 import com.example.felix.notizen.BackEnd.Logger.cNoteLogger;
 import com.example.felix.notizen.BackEnd.Logger.cNoteLoggerException;
 import com.example.felix.notizen.BackEnd.cBaseException;
@@ -17,50 +19,97 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    private cNoteLogger log;
+    private cNoteMaster noteMaster;
+    private cJsonManager jsonManager;
+    private int n = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cNoteLogger logger = cNoteLogger.getInstance();
-        try {
-            // delete logs from previous runs
-            String path = this.getApplicationContext().getFilesDir().getPath();
-            String[] files = this.getApplicationContext().fileList();
-            for (String file:files
-                 ) {
-                if (file.contains("NOTE_APPLICATION_LOG")) {
-                    getApplicationContext().deleteFile(file);
-                }
-            }
-            cContextManager cm = cContextManager.getInstance();
-            cm.setUp(this.getApplicationContext());
-            // init logger
-            logger.init(path, cNoteLogger.mDebugLevelInfo,10);
-            cTextNote textNote = new cTextNote(UUID.randomUUID(), "title", "message");
-            cImageNote imageNote = new cImageNote(UUID.randomUUID(), "t", "none");
-            logger.logInfo(textNote.aTYPE);
-            logger.logInfo(imageNote.aTYPE);
-            // test reading available JSON
-            cNoteMaster noteMaster = cNoteMaster.getInstance();
-            noteMaster.addNote(textNote);
-            cJsonManager reader = cJsonManager.getInstance();
-            reader.init(path+"/JSON_DATA.TXT");
-            reader.writeJSON();
+        // init vars
+        log = cNoteLogger.getInstance();
+        String path = this.getApplicationContext().getFilesDir().getPath();
+        log.init(path + "/logs",1, cNoteLogger.mDebugLevelDebug,10);
+        log.logInfo("onCreate");
+        jsonManager = cJsonManager.getInstance();
+        jsonManager.init(path+"/JSON_DATA.TXT");
+        noteMaster = cNoteMaster.getInstance();
 
-            reader.read_JSON();
-
-        }catch (cBaseException ex){
-            ex.logException();
-        }
-        finally {
-            try {
-                logger.flush();
-            } catch (cNoteLoggerException e) {
-                e.printStackTrace();
-            }
-        }
-
+        cTextNote textNote = new cTextNote(UUID.randomUUID(), "title", "message");
+        cImageNote imageNote = new cImageNote(UUID.randomUUID(), "t", "none");
+        // test reading available JSON
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        log.logInfo("onStart");
+        try {
+            jsonManager.read_JSON();
+        } catch (cJsonManagerException e) {
+            e.logException();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        log.logInfo("onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        log.logInfo("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        log.logInfo("onPause");
+        try {
+            jsonManager.writeJSON();
+            noteMaster.clear();
+        } catch (cJsonManagerException e) {
+            e.logException();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        log.logInfo("onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        log.logInfo("onDestroy");
+        try {
+            log.flush();
+        } catch (cNoteLoggerException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn1:
+                n++;
+                String s = String.valueOf(n);
+                cTextNote textNote = new cTextNote(UUID.randomUUID(), "title " + s, "message " + s);
+                noteMaster.addNote(textNote);
+                break;
+            case R.id.btn2:
+
+                break;
+        }
+
+    }
 }
