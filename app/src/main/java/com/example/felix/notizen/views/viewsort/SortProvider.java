98af54c0d0f6dd93dcd54bab7862e3c9c67369fb
FelixWiemann <felix.wiemann@web.de>
2019-09-12 18:58:07 +0200

    sorting by title ascending & descending
    + tests

--- app/src/main/java/com/example/felix/notizen/views/viewsort/SortProvider.java
+++ app/src/main/java/com/example/felix/notizen/views/viewsort/SortProvider.java
@@ -6,7 +6,10 @@ import java.util.Comparator;
 
 public class SortProvider {
 
-    public static final Comparator<cSortableObject> SortByTitle = new Comparator<cSortableObject>() {
+    /**
+     * Comparator for sorting the title ascending
+     */
+    public static final Comparator<cSortableObject> SortByTitleAscending = new Comparator<cSortableObject>() {
         @Override
         public int compare(cSortableObject t1, cSortableObject t2) {
             if (t1 == null){
@@ -16,6 +19,19 @@ public class SortProvider {
                 return 1;
             }
             return ((String)t1.getSortable(SortCategory.TITLE)).compareToIgnoreCase(((String)t2.getSortable(SortCategory.TITLE)));
+        }
+    };
+
+    public static final Comparator<cSortableObject> SortByTitleDescending = new Comparator<cSortableObject>() {
+        @Override
+        public int compare(cSortableObject t1, cSortableObject t2) {
+            if (t1 == null){
+                return 1;
+            }
+            if (t2 == null){
+                return -1;
+            }
+            return ((String)t2.getSortable(SortCategory.TITLE)).compareToIgnoreCase(((String)t1.getSortable(SortCategory.TITLE)));
         }
     };
 
--- app/src/test/java/com/example/felix/notizen/views/viewsort/SortProviderTest.java
+++ app/src/test/java/com/example/felix/notizen/views/viewsort/SortProviderTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class SortProviderTest {
 
     @Test
-    public void testSortByTitle(){
+    public void testSortByTitleAscending(){
         cTextNote object1 = null;
         cTextNote object2 = new cTextNote(UUID.randomUUID(), "aaa", "");
         cTextNote object3 = new cTextNote(UUID.randomUUID(), "aba", "");
@@ -20,15 +20,37 @@ public class SortProviderTest {
         cTextNote object6 = new cTextNote(UUID.randomUUID(), "", "");
         cTextNote object7 = new cTextNote(UUID.randomUUID(), "bbb", "");
 
-        assertTrue(SortProvider.SortByTitle.compare(object3, object2)>0);
-        assertTrue(SortProvider.SortByTitle.compare(object2, object3)<0);
-        assertTrue(SortProvider.SortByTitle.compare(object4, object2)>0);
-        assertTrue(SortProvider.SortByTitle.compare(object5, object2)>0);
-        assertTrue(SortProvider.SortByTitle.compare(object3, object6)>0);
-        assertTrue(SortProvider.SortByTitle.compare(object3, object1)>0);
-        assertTrue(SortProvider.SortByTitle.compare(object1, object3)<0);
-        assertEquals(0, SortProvider.SortByTitle.compare(object3, object3));
-        assertEquals(0, SortProvider.SortByTitle.compare(object4, object7));
+        assertTrue(SortProvider.SortByTitleAscending.compare(object3, object2)>0);
+        assertTrue(SortProvider.SortByTitleAscending.compare(object2, object3)<0);
+        assertTrue(SortProvider.SortByTitleAscending.compare(object4, object2)>0);
+        assertTrue(SortProvider.SortByTitleAscending.compare(object5, object2)>0);
+        assertTrue(SortProvider.SortByTitleAscending.compare(object3, object6)>0);
+        assertTrue(SortProvider.SortByTitleAscending.compare(object3, object1)>0);
+        assertTrue(SortProvider.SortByTitleAscending.compare(object1, object3)<0);
+        assertEquals(0, SortProvider.SortByTitleAscending.compare(object3, object3));
+        assertEquals(0, SortProvider.SortByTitleAscending.compare(object4, object7));
+
+    }
+
+    @Test
+    public void testSortByTitleDescending(){
+        cTextNote object1 = null;
+        cTextNote object2 = new cTextNote(UUID.randomUUID(), "aaa", "");
+        cTextNote object3 = new cTextNote(UUID.randomUUID(), "aba", "");
+        cTextNote object4 = new cTextNote(UUID.randomUUID(), "bbb", "");
+        cImageNote object5 = new cImageNote(UUID.randomUUID(), "abc", "");
+        cTextNote object6 = new cTextNote(UUID.randomUUID(), "", "");
+        cTextNote object7 = new cTextNote(UUID.randomUUID(), "bbb", "");
+
+        assertTrue(SortProvider.SortByTitleDescending.compare(object3, object2)<0);
+        assertTrue(SortProvider.SortByTitleDescending.compare(object2, object3)>0);
+        assertTrue(SortProvider.SortByTitleDescending.compare(object4, object2)<0);
+        assertTrue(SortProvider.SortByTitleDescending.compare(object5, object2)<0);
+        assertTrue(SortProvider.SortByTitleDescending.compare(object3, object6)<0);
+        assertTrue(SortProvider.SortByTitleDescending.compare(object3, object1)<0);
+        assertTrue(SortProvider.SortByTitleDescending.compare(object1, object3)>0);
+        assertEquals(0, SortProvider.SortByTitleDescending.compare(object3, object3));
+        assertEquals(0, SortProvider.SortByTitleDescending.compare(object4, object7));
 
     }
 
