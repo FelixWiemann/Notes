8ed7b8fe0df0782c3450f71ded94c3427289a7d2
FelixWiemann <felix.wiemann@web.de>
2019-09-17 20:07:23 +0200

    added gesture detection and appropriate anims for ListView
    custom list view now supports swipe detection and is prepared for new
    gestures.
    
    the swipes to the right and left will be animated on the list view items
    accordingly

--- app/src/main/java/com/example/felix/notizen/views/customListView.java
+++ app/src/main/java/com/example/felix/notizen/views/customListView.java
@@ -1,19 +1,16 @@
 package com.example.felix.notizen.views;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.provider.ContactsContract;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.felix.notizen.Utils.DBAccess.DatabaseStorable;
-import com.example.felix.notizen.Utils.DBAccess.cDBDataHandler;
 import com.example.felix.notizen.Utils.Logger.cNoteLogger;
-import com.example.felix.notizen.views.viewsort.FilterBasedOnClass;
 import com.example.felix.notizen.views.viewsort.ViewFilter;
 
 /**
@@ -23,10 +20,7 @@ import com.example.felix.notizen.views.viewsort.ViewFilter;
 
 public class customListView extends ListView {
 
-    // TODO move databaseinteractions to the adapter
     private cExpandableViewAdapter adapter = new cExpandableViewAdapter();
-
-    private cDBDataHandler handler;
 
     public customListView(Context context) {
         super(context);
@@ -48,40 +42,102 @@ public class customListView extends ListView {
         adapter.notifyDataSetChanged();
     }
 
+    public void remove(DatabaseStorable storable){
+        adapter.remove(storable);
+        adapter.notifyDataSetChanged();
+    }
+
     public void init(){
         this.setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS);
-        handler = new cDBDataHandler();
-        this.setOnItemLongClickListener(new OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(customListView.super.getContext(), "long click", Toast.LENGTH_LONG).show();
-                DatabaseStorable storable = adapter.getItem(position);
-                adapter.remove(storable);
-                handler.delete(storable);
-
-                adapter.notifyDataSetChanged();
-                return true;
-            }
-        });
-        this.setOnItemSelectedListener(new OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(customListView.super.getContext(), "on item selected", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Toast.makeText(customListView.super.getContext(), "on nothing selected", Toast.LENGTH_LONG).show();
-            }
-        });
-        this.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(customListView.super.getContext(), "short click", Toast.LENGTH_LONG).show();
-            }
-        });
         this.setAdapter(adapter);
-        cNoteLogger.getInstance().logInfo("customListview init");
+        cNoteLogger.getInstance().logInfo("customListView init");
+
+        this.setOnTouchListener(new OnTouchListener() {
+            final GestureDetector gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
+
+                @Override
+                public boolean onDown(MotionEvent motionEvent) {
+                    return customListView.this.onDown(motionEvent);
+                }
+
+                @Override
+                public void onLongPress(MotionEvent motionEvent) {
+                    customListView.this.onLongPress(motionEvent);
+
+                }
+
+                @Override
+                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                    return customListView.this.onFling(e1,e2,velocityX,velocityY);
+                }
+            });
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return gestureDetector.onTouchEvent(motionEvent);
+            }
+        });
+
+    }
+
+    private final int REL_SWIPE_MIN_DISTANCE = (int)(120.0f * getResources().getDisplayMetrics().densityDpi / 160.0f + 0.5);
+    private final int REL_SWIPE_MAX_OFF_PATH = (int)(250.0f * getResources().getDisplayMetrics().densityDpi / 160.0f + 0.5);
+    private final int REL_SWIPE_THRESHOLD_VELOCITY = (int)(200.0f * getResources().getDisplayMetrics().densityDpi / 160.0f + 0.5);
+
+    private View currentlyAnimated = null;
+    private float xBeforeAnimated;
+
+    private boolean onDown(MotionEvent motionEvent){
+        final View currentReturn;
+        if (currentlyAnimated != null && currentlyAnimated != getChildViewAtMotionEventPosition(motionEvent)){
+            currentReturn = currentlyAnimated;
+            currentlyAnimated = null;
+            ObjectAnimator animation = ObjectAnimator.ofFloat(currentReturn,"translationX", (0f));
+            animation.setDuration(200);
+            animation.start();
+
+        }
+        return false;
+    }
+
+    private void onLongPress(MotionEvent motionEvent) {
+        // TODO open context menu -> actions delete, edit, etc.
+    }
+
+    private boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if (Math.abs(e1.getY() - e2.getY()) > REL_SWIPE_MAX_OFF_PATH)
+            return false;
+
+        int direction = 0;
+
+        if(e1.getX() - e2.getX() > REL_SWIPE_MIN_DISTANCE &&
+                Math.abs(velocityX) > REL_SWIPE_THRESHOLD_VELOCITY) {
+            direction = -1;
+        }  else if (e2.getX() - e1.getX() > REL_SWIPE_MIN_DISTANCE &&
+                Math.abs(velocityX) > REL_SWIPE_THRESHOLD_VELOCITY) {
+            direction = 1;
+        }
+        currentlyAnimated = getChildViewAtMotionEventPosition(e1);
+        if (currentlyAnimated !=null){
+            xBeforeAnimated = currentlyAnimated.getTranslationX();
+            ObjectAnimator animation = ObjectAnimator.ofFloat(currentlyAnimated,"translationX", 130f*direction);
+            animation.setDuration(400);
+            animation.start();
+        }
+        return false;
+    }
+
+    private View getChildViewAtMotionEventPosition(MotionEvent event){
+        if (event == null){
+            return null;
+        }
+        return this.getChildAt(this.pointToPosition((int)event.getX(),(int)event.getY()));
+    }
+
+    @Override
+    public boolean performClick(){
+        onDown(null);
+        return super.performClick();
     }
 
     public void filter(ViewFilter filter) {
