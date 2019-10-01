f731fddaa45e6323d78a987f95d7bccfd98a693f
FelixWiemann <felix.wiemann@web.de>
2019-10-01 23:22:27 +0200

    added Header Fragment to EditNoteActivity
    the fragment shall contain all info that can be changed in any note.
    e.g. title
    
    the title is currently the only thing that is being editable in here

--- app/src/main/java/com/example/felix/notizen/EditNoteActivity.java
+++ app/src/main/java/com/example/felix/notizen/EditNoteActivity.java
@@ -1,12 +1,15 @@
 package com.example.felix.notizen;
 
 import android.app.Activity;
+import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +17,11 @@ import android.widget.EditText;
 import com.example.felix.notizen.Utils.DBAccess.DatabaseStorable;
 import com.example.felix.notizen.objects.Notes.cTextNote;
 import com.example.felix.notizen.objects.StoragePackerFactory;
+import com.example.felix.notizen.objects.cIdObject;
 import com.example.felix.notizen.views.fragments.EditNoteViewModel;
 import com.example.felix.notizen.views.fragments.NoteDisplayFragment;
 import com.example.felix.notizen.views.fragments.NoteDisplayFragmentFactory;
+import com.example.felix.notizen.views.fragments.NoteDisplayHeaderFragment;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -24,16 +29,16 @@ import java.util.UUID;
 
 public class EditNoteActivity extends AppCompatActivity {
 
-    private EditText etTitle;
     private Button btSave;
 
     private DatabaseStorable data;
+
+    private EditNoteViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
-        etTitle = findViewById(R.id.etTitle);
         btSave = findViewById(R.id.btSave);
         btSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,15 +56,15 @@ public class EditNoteActivity extends AppCompatActivity {
         if (data == null){
             data = new cTextNote(UUID.randomUUID(),"new note", "new message");
         }
-        EditNoteViewModel mViewModel = ViewModelProviders.of(this).get(EditNoteViewModel.class);
+        mViewModel = ViewModelProviders.of(this).get(EditNoteViewModel.class);
         mViewModel.setNote(data);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        NoteDisplayFragment fragment = NoteDisplayFragmentFactory.generateFragment(data);
+        NoteDisplayFragment fragment = new NoteDisplayHeaderFragment();
+        fragmentTransaction.add(R.id.fragementHeader, fragment);
+        fragment = NoteDisplayFragmentFactory.generateFragment(data);
         fragmentTransaction.add(R.id.fragementHolder, fragment);
         fragmentTransaction.commit();
-
-        etTitle.setText(data.getType());
     }
 
     private void save(){
