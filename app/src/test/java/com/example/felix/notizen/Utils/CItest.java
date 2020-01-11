ee0fa9a952bc140e8ae07fa5a07d8d40f16c76cd
Felix Wiemann <13033994+FelixWiemann@users.noreply.github.com>
2020-01-11 18:04:45 +0100

    test if ci fails

---
+++ app/src/test/java/com/example/felix/notizen/Utils/CItest.java
@@ -0,0 +1,13 @@
+package com.example.felix.notizen.Utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class CItest {
+
+    @Test
+    public void test(){
+        fail("want to test whether CI Azure fails");
+    }
+}
