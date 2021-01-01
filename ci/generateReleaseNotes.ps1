9ed92ac43a4c6b19b47758777643bfcf9d0869e8
Felix Wiemann <13033994+FelixWiemann@users.noreply.github.com>
2021-01-02 00:44:59 +0100

    additional logging for ci ps script.
    removed en-US tags from generated releasenotes

--- ci/generateReleaseNotes.ps1
+++ ci/generateReleaseNotes.ps1
@@ -4,24 +4,23 @@ param()
 $AzureDevOpsAuthenicationHeader = @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }
 
 $CurrentIterApiCall = "https://dev.azure.com/nepumuk/Notes/Notes%20Team/_apis/work/teamsettings/iterations?$timeframe=current&api-version=6.0"
-
 $Result = Invoke-RestMethod -Uri $CurrentIterApiCall -Method get -Headers $AzureDevOpsAuthenicationHeader
-
 $CurrentIterId=$Result.Value.id
+
+Write-Host "got current iteration " $Result.Value.name " with id " $CurrentIterId
 
 $Path = "./ci/ReleaseNotes.md"
 
-#Write-Host "Iteration" $Result.Value.name "id: " $CurrentIterId
 $CurrentIterWiApiCall="https://dev.azure.com/nepumuk/Notes/_apis/work/teamsettings/iterations/$CurrentIterId/workitems?api-version=6.0-preview.1"
-#Write-Host "API:" $CurrentIterWiApiCall
 $Result = Invoke-RestMethod -Uri $CurrentIterWiApiCall -Method GET -Headers $AzureDevOpsAuthenicationHeader
 
-#$Result.workItemRelations
+Write-Host "got workitems for iteration " $Result.Value.name
 
 $FixedBugs=@()
 $NewFeatures=@()
 $NewImprovements=@()
 
+Write-Host "analyzing workitems"
 foreach ($WiRelation in $Result.workItemRelations) {
 	$WiId=$WiRelation.target.id
 	$WiApiCall="https://dev.azure.com/nepumuk/Notes/_apis/wit/workItems/$WiId"
@@ -38,7 +37,9 @@ foreach ($WiRelation in $Result.workItemRelations) {
 		}
 	}
 }
-"<en-US>" | Set-Content -Path $Path
+Write-Host "creating release notes"
+
+# "<en-US>" | Set-Content -Path $Path
 if ($NewFeatures.Length -gt 0){
 	"#New Features" | Add-Content -Path $Path
 	foreach ($Feature in $NewFeatures) {
@@ -60,4 +61,7 @@ if ($FixedBugs.Length -gt 0){
 		"*" + $Bugfix.SubString(5,$Bugfix.length - 5-6)  | Add-Content -Path $Path
 	}
 }
-"</en-US>" | Add-Content -Path $Path
+# "</en-US>" | Add-Content -Path $Path
+
+
+Write-Host "done..."
