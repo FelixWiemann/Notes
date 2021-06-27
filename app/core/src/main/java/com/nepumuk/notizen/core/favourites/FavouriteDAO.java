3fcede9e151d649aa789fe89cc1c38e851c18a5a
Felix Wiemann <13033994+FelixWiemann@users.noreply.github.com>
2021-06-27 18:22:26 +0200

    changed favouritedao for easier use

--- app/core/src/main/java/com/nepumuk/notizen/core/favourites/FavouriteDAO.java
+++ app/core/src/main/java/com/nepumuk/notizen/core/favourites/FavouriteDAO.java
@@ -4,22 +4,61 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
+import androidx.room.Update;
 
 import java.util.List;
 
 @Dao
-public interface FavouriteDAO {
+public abstract class FavouriteDAO {
 
+    /**
+     * gets all stored favourites
+     * @return list of all favourites
+     */
     @Query("SELECT * FROM favourite")
-    List<Favourite> getAll();
+    public abstract List<Favourite> getAll();
 
+    /**
+     * returns a specific favourite specified by id
+     * @param uuid
+     * @return
+     */
     @Query("SELECT * FROM favourite WHERE note_id IN (:uuid) LIMIT 1")
-    Favourite findFavourite(String uuid);
+    public abstract Favourite findFavourite(String uuid);
 
+
+    /**
+     * inserts all given favourites
+     * @param favourites
+     */
     @Insert
-    void insertAll(Favourite... favourites);
+    public abstract void insertAll(Favourite... favourites);
 
+    /**
+     * favourites to delete
+     * @param favourites to delete
+     */
     @Delete
-    void delete(Favourite favourite);
+    public abstract void delete(Favourite... favourites);
+
+    /**
+     * update the given favourites
+     * @param favourites to update
+     * @return number of favourites changed
+     */
+    @Update
+    public abstract int update(Favourite... favourites);
+
+    /**
+     * creates or updates a favourite in the database depending of whether it exists or not
+     * @param favourite to update or create
+     */
+    @Transaction()
+    public void createOrUpdate(Favourite favourite){
+        if (update(favourite)==0){
+            insertAll(favourite);
+        }
+    }
 
 }
